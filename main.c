e0060b3ed0fe40def8c4c72bb6698681f26b01c5
TheoTheTorch <theothetorch@gmail.com>
2025-11-13 14:55:52 +0100

    Recieve expression from arguments

--- main.c
+++ main.c
@@ -1,21 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 #include "lexer.h"
 
 int main(int argc, char **argv) {
-    char example_input[] = "129.45       + - * / % 34.995\n\0";
-    // int token_count;
-    // Token *tokens = lexer(example_input, &token_count);
-
-    // for (int i = 0; i < token_count; i++)
-    // {
-    //     printf("Value: %s\n", tokens[i].lexeme);
-    // }
-
-    // free(tokens);
+    
+    assert(argc == 2 && "Must provide (exactly one) mathematical expression as argument.");
+    char* expression = (char*) argv[1];
 
     Lexer lexer;
-    lexer_initialize(&lexer, &example_input[0]);
+    lexer_initialize(&lexer, &expression[0]);
 
     Token tokens[128];
     int i = 0;
