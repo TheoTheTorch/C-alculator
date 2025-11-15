737607f5381d43d31cfbab196de57be38048716c
TheoTheTorch <theothetorch@gmail.com>
2025-11-15 16:51:12 +0100

    Amend: forgot to delete lines in lexer.h

--- lexer.h
+++ lexer.h
@@ -8,8 +8,6 @@ typedef struct Lexer
     char* current;
 } Lexer;
 
-// Lexer functions
-// ---------------
 void lexer_initialize(Lexer* lexer, char* expression_start);
 
 void lexer_advance(Lexer* lexer);
@@ -21,15 +19,5 @@ Token lexer_number(Lexer* lexer);
 Token lexer_create_token(Lexer* lexer, TokenType type);
 
 Token lexer_next_token(Lexer* lexer);
-// ---------------
-
-// Tokenizer utilities
-// -------------------
-Token* tokenize(char* expression);
-
-void free_tokens(Token* token);
-
-void print_tokens(Token* tokens);
-// -------------------
 
 #endif
